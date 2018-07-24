588cd5f57f3b89aa5c6dc09bf9a851db9316d218
sirisha6978 <siri.sirisha6979@gmail.com>
2018-07-24 17:35:05 +0530

    first commit by sree

---
+++ src/sample_package/Sample.java
@@ -0,0 +1,16 @@
+package sample_package;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println("welcome to hithub repositary");
+		
+		
+	}
+	
+	
+	
+	
+
+}
